acab942538d8f8e616864c8088e0bd7e0ab0365f
thandar <sk.kizzy.tkz@gmail.com>
2024-06-08 16:26:10 +0700

    add DailyAttendanceEntity to the model

---
+++ ASPNet8/CloudHRMS/Models/DataModels/DailyAttendanceEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CloudHRMS.Models.DataModels
+{
+    [Table("DailyAttendance")]
+    public class DailyAttendanceEntity:BaseEntity
+    {
+        public DateTime AttendanceDate { get; set; }
+        public TimeSpan InTime { get; set; }
+        public TimeSpan OutTime { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public string EmployeeId { get; set; }
+        public virtual EmployeeEntity Employee { get; set; }
+        [ForeignKey("DepartmentId")]
+        public string DepartmentId { get; set; }
+        public virtual DepartmentEntity Department { get; set; }
+
+    }
+}
